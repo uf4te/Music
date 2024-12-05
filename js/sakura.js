173a68fe96c9b426f736c41c7aab31ad4367bb66
uf4te <66998280+uf4te@users.noreply.github.com>
2024-12-05 10:55:31 +0800

    Update sakura.js

--- js/sakura.js
+++ js/sakura.js
@@ -118,7 +118,7 @@ function startSakura() {
     document.getElementsByTagName('body')[0].appendChild(canvas);
     cxt = canvas.getContext('2d');
     var sakuraList = new SakuraList();
-    for (var i = 0; i < 4; i++) {   // 循环次数控制樱花数量
+    for (var i = 0; i < 15; i++) {   // 循环次数控制樱花数量
         var sakura, randomX, randomY, randomS, randomR, randomFnx, randomFny;
         randomX = getRandom('x');
         randomY = getRandom('y');
